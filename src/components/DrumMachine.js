5b0df563c214d3272829c3ed695dbed9a06c1052
adra3n <srtc.kcgl@gmail.com>
2023-07-06 16:03:29 +0300

    fix responsive buttons

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -61,6 +61,11 @@ const DrumMachine = () => {
           className={`drum-pad btn btn-primary ${
             activeButton === 'drum-Q' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-0"
           onClick={() => play('Q')}
           onMouseEnter={() => handleButtonHover('drum-Q')}
@@ -74,6 +79,11 @@ const DrumMachine = () => {
           className={`drum-pad btn btn-primary ${
             activeButton === 'drum-W' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-1"
           onClick={() => play('W')}
           onMouseEnter={() => handleButtonHover('drum-W')}
@@ -87,6 +97,11 @@ const DrumMachine = () => {
           className={`drum-pad btn btn-primary ${
             activeButton === 'drum-E' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-2"
           onClick={() => play('E')}
           onMouseEnter={() => handleButtonHover('drum-E')}
@@ -103,6 +118,11 @@ const DrumMachine = () => {
           className={`drum-pad btn btn-success ${
             activeButton === 'drum-A' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-3"
           onClick={() => play('A')}
           onMouseEnter={() => handleButtonHover('drum-A')}
@@ -116,6 +136,11 @@ const DrumMachine = () => {
           className={`drum-pad btn btn-success ${
             activeButton === 'drum-S' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-4"
           onClick={() => play('S')}
           onMouseEnter={() => handleButtonHover('drum-S')}
@@ -129,55 +154,76 @@ const DrumMachine = () => {
           className={`drum-pad btn btn-success ${
             activeButton === 'drum-D' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-5"
           onClick={() => play('D')}
           onMouseEnter={() => handleButtonHover('drum-D')}
           onMouseLeave={handleButtonLeave}
         >
           <p>D</p>
-          <p>--- Hi-Hat ---</p>
+          <p>Hi-Hat</p>
           <audio src={require('../assets/D.mp3')} className="clip" id="D" />
         </button>
       </div>
       <br />
       <div className="text-center">
         <button
-          className={`drum-pad btn btn-warning ${
+          className={`drum-pad btn btn-danger ${
             activeButton === 'drum-Z' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-6"
           onClick={() => play('Z')}
           onMouseEnter={() => handleButtonHover('drum-Z')}
           onMouseLeave={handleButtonLeave}
         >
           <p>D</p>
-          <p>--- Ride ---</p>
+
+          <p>Ride</p>
           <audio src={require('../assets/Z.mp3')} className="clip" id="Z" />
         </button>
         <button
-          className={`drum-pad btn btn-warning ${
+          className={`drum-pad btn btn-danger ${
             activeButton === 'drum-X' && 'active'
           }`}
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           id="drum-7"
           onClick={() => play('X')}
           onMouseEnter={() => handleButtonHover('drum-X')}
           onMouseLeave={handleButtonLeave}
         >
           <p>X</p>
-          <p>--- Kick ---</p>
+          <p>Kick</p>
           <audio src={require('../assets/X.mp3')} className="clip" id="X" />
         </button>
         <button
-          className={`drum-pad btn btn-warning ${
+          className={`drum-pad btn btn-danger ${
             activeButton === 'drum-C' && 'active'
           }`}
           id="drum-8"
+          style={{
+            fontWeight: 'bold',
+            fontSize: '18px',
+            paddingTop: '0.5rem',
+          }}
           onClick={() => play('C')}
           onMouseEnter={() => handleButtonHover('drum-C')}
           onMouseLeave={handleButtonLeave}
         >
           <p>C</p>
-          <p>--- Bell ---</p>
+          <p>Bell</p>
           <audio src={require('../assets/C.mp3')} className="clip" id="C" />
         </button>
       </div>
