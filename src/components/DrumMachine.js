63a1219de5593dc3ad8e161b35b88b3a97eb430f
adra3n <srtc.kcgl@gmail.com>
2023-07-06 15:13:51 +0300

    hover effect on key press

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
 const DrumMachine = () => {
-  const [displayText, setDisplayText] = useState('Click a button to play!')
+  const [displayText, setDisplayText] = useState('Click a button!')
+  const [activeButton, setActiveButton] = useState('')
 
   const play = (str) => {
     setDisplayText(str)
 
     const audio = new Audio(require(`../assets/${str}.mp3`))
     audio.play()
+  }
+
+  const handleButtonHover = (buttonId) => {
+    setActiveButton(buttonId)
+  }
+
+  const handleButtonLeave = () => {
+    setActiveButton('')
   }
 
   useEffect(() => {
@@ -27,6 +36,7 @@ const DrumMachine = () => {
         keystroke === 'C'
       ) {
         play(keystroke)
+        setActiveButton(`drum-${keystroke}`)
       }
     }
 
@@ -49,81 +59,115 @@ const DrumMachine = () => {
       </div>
       <div className="align-items-center text-center">
         <button
-          className="drum-pad btn  btn-primary"
+          className={`drum-pad btn btn-primary ${
+            activeButton === 'drum-Q' && 'active'
+          }`}
           id="drum-0"
           onClick={() => play('Q')}
+          onMouseEnter={() => handleButtonHover('drum-Q')}
+          onMouseLeave={handleButtonLeave}
         >
           Q
           <audio src={require('../assets/Q.mp3')} className="clip" id="Q" />
         </button>
         <button
-          className="drum-pad btn  btn-primary"
+          className={`drum-pad btn btn-primary ${
+            activeButton === 'drum-W' && 'active'
+          }`}
           id="drum-1"
           onClick={() => play('W')}
+          onMouseEnter={() => handleButtonHover('drum-W')}
+          onMouseLeave={handleButtonLeave}
         >
           W
           <audio src={require('../assets/W.mp3')} className="clip" id="W" />
         </button>
         <button
-          className="drum-pad btn  btn-primary"
+          className={`drum-pad btn btn-primary ${
+            activeButton === 'drum-E' && 'active'
+          }`}
           id="drum-2"
           onClick={() => play('E')}
+          onMouseEnter={() => handleButtonHover('drum-E')}
+          onMouseLeave={handleButtonLeave}
         >
           E
           <audio src={require('../assets/E.mp3')} className="clip" id="E" />
         </button>
       </div>
-      <br />
-
+      <br />{' '}
       <div className="text-center">
         <button
-          className="drum-pad btn  btn-success"
+          className={`drum-pad btn btn-success ${
+            activeButton === 'drum-A' && 'active'
+          }`}
           id="drum-3"
           onClick={() => play('A')}
+          onMouseEnter={() => handleButtonHover('drum-A')}
+          onMouseLeave={handleButtonLeave}
         >
           A
           <audio src={require('../assets/A.mp3')} className="clip" id="A" />
         </button>
         <button
-          className="drum-pad btn  btn-success"
+          className={`drum-pad btn btn-success ${
+            activeButton === 'drum-S' && 'active'
+          }`}
           id="drum-4"
           onClick={() => play('S')}
+          onMouseEnter={() => handleButtonHover('drum-S')}
+          onMouseLeave={handleButtonLeave}
         >
           S
           <audio src={require('../assets/S.mp3')} className="clip" id="S" />
         </button>
         <button
-          className="drum-pad btn  btn-success"
+          className={`drum-pad btn btn-success ${
+            activeButton === 'drum-D' && 'active'
+          }`}
           id="drum-5"
           onClick={() => play('D')}
+          onMouseEnter={() => handleButtonHover('drum-D')}
+          onMouseLeave={handleButtonLeave}
         >
           D
           <audio src={require('../assets/D.mp3')} className="clip" id="D" />
         </button>
       </div>
       <br />
-
       <div className="text-center">
         <button
-          className="drum-pad btn  btn-warning"
+          className={`drum-pad btn btn-warning ${
+            activeButton === 'drum-Z' && 'active'
+          }`}
           id="drum-6"
           onClick={() => play('Z')}
+          onMouseEnter={() => handleButtonHover('drum-Z')}
+          onMouseLeave={handleButtonLeave}
         >
           Z
           <audio src={require('../assets/Z.mp3')} className="clip" id="Z" />
         </button>
         <button
-          className="drum-pad btn  btn-warning"
+          className={`drum-pad btn btn-warning ${
+            activeButton === 'drum-X' && 'active'
+          }`}
           id="drum-7"
           onClick={() => play('X')}
+          onMouseEnter={() => handleButtonHover('drum-X')}
+          onMouseLeave={handleButtonLeave}
         >
           X
           <audio src={require('../assets/X.mp3')} className="clip" id="X" />
         </button>
         <button
-          className="drum-pad btn  btn-warning"
+          className={`drum-pad btn btn-warning ${
+            activeButton === 'drum-C' && 'active'
+          }`}
           id="drum-8"
           onClick={() => play('C')}
+          onMouseEnter={() => handleButtonHover('drum-C')}
+          onMouseLeave={handleButtonLeave}
         >
           C
           <audio src={require('../assets/C.mp3')} className="clip" id="C" />
